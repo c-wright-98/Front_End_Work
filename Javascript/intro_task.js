416599a0feb9554e4e4da0c8b0e19c4a90f5a69b
c-wright-98 <corvus.wright@gmail.com>
2023-08-02 16:17:35 +0100

    fixed the back tick

--- Javascript/intro_task.js
+++ Javascript/intro_task.js
@@ -5,6 +5,6 @@ console.error("Sagittarius");
 
 let make = "car make";
 let model = "car model";
-console.log('my favourite car make is ${make} and the model is ${model}')
+console.log(`my favourite car make is ${make} and the model is ${model}`)
 
 console.log("This is the last task", "color: red; font-family: fantasy; font-style: Bold; background-color: black; padding: 10px");
