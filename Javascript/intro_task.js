4c95fae79ec43e14848cc34e26625187143d1b3e
c-wright-98 <corvus.wright@gmail.com>
2023-08-02 16:12:51 +0100

    Transferred file over from branch

---
+++ Javascript/intro_task.js
@@ -0,0 +1,10 @@
+console.log("Corvus");
+console.info("Wright");
+console.warn("England");
+console.error("Sagittarius");
+
+let make = "car make";
+let model = "car model";
+console.log('my favourite car make is ${make} and the model is ${model}')
+
+console.log("This is the last task", "color: red; font-family: fantasy; font-style: Bold; background-color: black; padding: 10px");
