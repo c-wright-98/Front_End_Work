02110e890538f4e1ce1e5cd255b78b01a33ac1d9
c-wright-98 <corvus.wright@gmail.com>
2023-08-03 10:10:48 +0100

    Initial Commit

---
+++ Javascript/variables.js
@@ -0,0 +1,11 @@
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay;
+
+`The total bill is £4000 the remaining amount of money to be paid is £1652`
