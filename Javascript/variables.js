90d1541b239cb9bc0902c93e9b1b39af430d35f3
c-wright-98 <corvus.wright@gmail.com>
2023-08-03 10:15:26 +0100

    Completed variables task

--- Javascript/variables.js
+++ Javascript/variables.js
@@ -4,8 +4,14 @@ let c = 12344;
 let d = true;
 let e = {a:"JavaScript"};
 
+typeof(a);
+typeof(b);
+typeof(c);
+typeof(d);
+typeof(e);
+
 let totalMoney = 4000;
 let moneyPaidSoFar = 2348;
-let totalLeftToPay;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
 
-`The total bill is £4000 the remaining amount of money to be paid is £1652`
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`)
