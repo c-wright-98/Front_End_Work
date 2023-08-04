965da74d42d59a0b6d148dfd26d34b2e78d4c660
c-wright-98 <corvus.wright@gmail.com>
2023-08-04 12:13:24 +0100

    Completed tasks

---
+++ Javascript/functions.js
@@ -0,0 +1,18 @@
+function subNum(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(subNum(10,5));
+
+function welcome(name,age,gender) {
+    return console.log(`My name is ${name}, I am ${age} years old and of gender ${gender}`);
+}
+
+console.log(welcome("Corvus Wright", 24, "Male"));
+
+function powerUp(n1,n2) {
+    let result = Math.pow(n1,n2)
+    return result;
+}
+
+console.log(powerUp(10,10));
