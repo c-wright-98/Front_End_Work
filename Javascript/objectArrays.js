3e40a171562aac5cdba3b646a8c831e0df013fcf
c-wright-98 <corvus.wright@gmail.com>
2023-08-04 10:56:23 +0100

    Fixed unqouted text amd added another array length check

--- Javascript/objectArrays.js
+++ Javascript/objectArrays.js
@@ -8,14 +8,15 @@ console.log(darthVader);
 
 console.log(`Darth Vader is allied to the ${darthVader.allegiance}`);
 console.log(`Darth Vader's weapon is a ${darthVader.weapon}`);
-console.log(`Is Darth Vader a sith ${darthVader.sith ? Yes : No}`);
-console.log(`Is Darth Vader a jedi ${darthVader.sith ? No : Yes}`);
+console.log(`Is Darth Vader a sith? ${darthVader.sith ? "Yes" : "No"}`);
+console.log(`Is Darth Vader a jedi? ${darthVader.sith ? "No" : "Yes"}`);
 
 let myArray = ["hello", "everyone"];
 console.log(myArray.length);
 myArray.push("goodbye");
 console.log(myArray.length);
 myArray.shift();
+console.log(myArray.length);
 
 for (let element of myArray){
     console.log(element);
